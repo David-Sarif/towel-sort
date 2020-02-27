a949b696a3cc41e20119db9eeaf195a088e771b7
David-Sarif <DavidSarif@yandex.ru>
2020-02-27 07:26:55 +0300

    feat:towel sort implemented

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  // Checking for empty input
+  if (!matrix)
+     return []
+
+  let towel=[];
+
+  for (let i=0;i<matrix.length;i++){
+    // taking every even subarray and adding to our new one
+    if (i%2==0)
+      towel=towel.concat(matrix[i]);
+
+    else
+    // every odd one should be reversed and then added
+       towel=towel.concat(matrix[i].reverse());
+  
+    }
+  return towel
 }
